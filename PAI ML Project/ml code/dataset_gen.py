0f227b26304c1afd97c222c470e45d7b13552edd
Muhammad Haris <139040989+malikharis01@users.noreply.github.com>
2026-04-19 11:28:10 +0500

    update

--- PAI ML Project/ml code/dataset_gen.py
+++ PAI ML Project/ml code/dataset_gen.py
@@ -3,7 +3,7 @@ import random
 
 data = []
 
-# Generate 1000 records
+
 for i in range(1, 1001):
     month = (i % 12) + 1
 
@@ -14,17 +14,17 @@ for i in range(1, 1001):
     bills = random.randint(5000, 9000)
     income = random.randint(25000, 50000)
 
-    # Total expense (with small randomness)
+ 
     total_expense = food + travel + shopping + bills + random.randint(-500, 500)
 
     data.append([month, food, travel, shopping, bills, income, total_expense])
 
-# Create DataFrame
+
 df = pd.DataFrame(data, columns=[
     "month", "food", "travel", "shopping", "bills", "income", "total_expense"
 ])
 
-# Save to CSV
+
 df.to_csv("monthly_expenses.csv", index=False)
 
 print("Dataset created successfully with 1000 records!")
